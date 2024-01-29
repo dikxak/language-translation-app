e192c06cf80e6fae3f5c089ecaba9cd49f4fb9bf
dikxak <dikshakpoudel76@gmail.com>
2024-01-29 20:59:11 +0545

    minor ui improvements and minor config setup

--- src/components/TranslatedTextContainer.tsx
+++ src/components/TranslatedTextContainer.tsx
@@ -2,14 +2,26 @@ import { useState } from 'react';
 
 import { TranslatedContainerStyles } from '@/types';
 
-import { DEFAULT_TRANSLATED_TEXT_CONTAINER_STYLES } from '@/constants';
+import {
+  DEFAULT_TRANSLATED_TEXT_CONTAINER_STYLES,
+  FONT_SIZE_CONFIGURATION,
+} from '@/constants';
 
-const TranslatedTextContainer = (): React.JSX.Element => {
+interface TranslatedTextContainerProps {
+  translatedText: string;
+  translating: boolean;
+}
+
+const TranslatedTextContainer = ({
+  translatedText,
+  translating,
+}: TranslatedTextContainerProps): React.JSX.Element => {
   const [containerStyles, setContainerStyles] =
     useState<TranslatedContainerStyles>(
       DEFAULT_TRANSLATED_TEXT_CONTAINER_STYLES,
     );
 
+  const { max, min, step } = FONT_SIZE_CONFIGURATION;
   const { fontSize, color, backgroundColor } = containerStyles;
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,52 +31,70 @@ const TranslatedTextContainer = (): React.JSX.Element => {
     setContainerStyles({ ...containerStyles, [name]: inputValue });
   };
 
+  const isStartTranslationMsgVisible = !translating && !translatedText;
+
   return (
     <>
       <h2 className="secondary-heading">Output</h2>
       <div className="translated-text-container">
-        <div className="customization-container">
-          <label htmlFor="fontSize">
-            Update font size ({`${fontSize}px`})
-            <div>
+        {translatedText && (
+          <div className="customization-container">
+            <label htmlFor="fontSize">
+              Update font size ({`${fontSize}px`})
+              <div>
+                <input
+                  id="fontSize"
+                  name="fontSize"
+                  type="range"
+                  min={min}
+                  max={max}
+                  step={step}
+                  value={fontSize}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+
+            <label htmlFor="textColor">
+              Update text color
               <input
-                id="fontSize"
-                name="fontSize"
-                type="range"
-                min="16"
-                max="64"
-                step="4"
-                value={fontSize}
+                id="textColor"
+                name="color"
+                type="color"
+                value={color}
                 onChange={handleInputChange}
               />
-            </div>
-          </label>
+            </label>
 
-          <label htmlFor="textColor">
-            Update text color
-            <input
-              id="textColor"
-              name="color"
-              type="color"
-              value={color}
-              onChange={handleInputChange}
-            />
-          </label>
-
-          <label htmlFor="backgroundColor">
-            Update background
-            <input
-              id="backgroundColor"
-              name="backgroundColor"
-              type="color"
-              value={backgroundColor}
-              onChange={handleInputChange}
-            />
-          </label>
-        </div>
-        <div style={containerStyles} className="translated-text">
+            <label htmlFor="backgroundColor">
+              Update background
+              <input
+                id="backgroundColor"
+                name="backgroundColor"
+                type="color"
+                value={backgroundColor}
+                onChange={handleInputChange}
+              />
+            </label>
+          </div>
+        )}
+        <div
+          style={containerStyles}
+          className={`translated-text ${translatedText ? 'ellipse-open-animation' : ''}`}
+        >
           Hola!
         </div>
+
+        {isStartTranslationMsgVisible && (
+          <p className="start-translation-msg">Start translating words!</p>
+        )}
+
+        {translating && (
+          <div className="loader-container">
+            <div className="loader" />
+            <span>Translating</span>
+          </div>
+        )}
       </div>
     </>
   );
