1bd9ea2b38e87d7e2ae49dab40443e52bc733e60
dikxak <dikshakpoudel76@gmail.com>
2024-01-29 12:37:45 +0545

    finish translate button and it's container design

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,45 @@
-// eslint-disable-next-line import/prefer-default-export
+import type { TranslationLanguage } from '@/types';
+
 export const DEFAULT_TRANSLATION_CONFIG = {
   translationOption: 'auto',
   isMultipleWordsTranslated: false,
 };
+
+export const TRANSLATION_LANGUAGES: TranslationLanguage[] = [
+  {
+    id: 1,
+    language: '🇳🇵Nepali',
+  },
+  {
+    id: 2,
+    language: '🇪🇸 Spanish',
+  },
+  {
+    id: 3,
+    language: '🇩🇪 German',
+  },
+  {
+    id: 4,
+    language: ' 🇨🇳 Chinese',
+  },
+  {
+    id: 5,
+    language: '🇮🇳 Hindi',
+  },
+  {
+    id: 6,
+    language: '🇮🇹 Italian',
+  },
+  {
+    id: 7,
+    language: '🇸🇪 Swedish',
+  },
+  {
+    id: 8,
+    language: '🇯🇵 Japanese',
+  },
+  {
+    id: 9,
+    language: '🇵🇹 Portuguese',
+  },
+];
